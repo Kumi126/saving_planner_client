1bbf6f8cd549f80e3418b290c5c941546b776156
Kumi126 <kumi126927@gmail.com>
2024-04-03 23:29:14 +0100

    fine working ver

--- app.py
+++ app.py
@@ -1,82 +1,99 @@
 import streamlit as st
-from app.new_schema import SimulateCondition,Account
+from app.new_schema import SimulateCondition,Account,Interest,Bonus
 
 
 def add_accounts():
+
     count = 1
     intr_c = 1
-    bns_c = 0
-
-    def add_account(count):
-        
+    accounts:list[Account] = []
+    def add_account(count,intr_c):
+        interests = []
         def add_interest(intr_c,link_account:Account):
-            __ = link_account.Interest()
-            col1_1, col1_2, col1_3 = st.columns(3)
-            with col1_1:
-                __.balance_tier = st.number_input(key=f'balance_tier_{intr_c}', label='Balance tires')
-            with col1_2:
-                __.rate = st.number_input(key=f'rate_{intr_c}', label='Interest rate')
-            with col1_3:
-                __.limited_period = int(st.number_input(key=f'limited_period_{intr_c}', label='For How many month?'))
+            st.write(intr_c)
+            interest = Interest()
+            col2_1, col2_2, col2_3 = st.columns(3)
+            with col2_1:
+                interest.balance_tier = st.number_input(key=f'balance_tier_{intr_c}', label='Balance tires')
+            with col2_2:
+                interest.rate = st.number_input(key=f'rate_{intr_c}', label='Interest rate')
+            with col2_3:
+                interest.limited_period = int(st.number_input(key=f'limited_period_{intr_c}', label='For How many month?'))
             
-            key = str(intr_c - 1)
-            link_account.interests['%s' %key] = {
-                'tier': __.balance_tier,
-                'rate': __.rate,
-                'limited_period': __.limited_period
-            }
-            
-            if intr_c <= max_interest_condition:
+            interests.append(interest)
+            if len(interests) < max_interest_condition:
                 more_intr = st.toggle(key=f'interest_toggle_{intr_c}',label=f'Add more interest conditions')
                 if more_intr:
                     intr_c += 1
                     add_interest(intr_c,link_account)
+            # else:
+            #     intr_c += 1
         
-        st.write('#### Accounts')            
-        _ = Account(st.text_input(key=f'account_{count}', label=f'Account name{count}'))
-        _.balance = st.number_input(key=f'balance_{count}', label='Account balance')
-        _.monthly_add = st.number_input(key=f'monthly_add_{count}', label='How much will you pay in each month?')
+        col1_1, col1_2, col1_3 = st.columns(3)
+        with col1_1:     
+            acc = Account(st.text_input(key=f'account_{count}', label=f'Account name{count}'))
+        with col1_2:     
+            acc.balance = st.number_input(key=f'balance_{count}', label='Account balance')
+        with col1_3:     
+            acc.monthly_add = st.number_input(key=f'monthly_add_{count}', label='Monthly adding amount')
         
         st.write('##### Tell about interest')
-        add_interest(intr_c,_)
+        acc.interest_periodicity = str(st.radio(key=f'perid_{count}',label='Recieve interest', options=['Monthly', 'Annually']))
+        add_interest(intr_c,acc)
+        acc.interests = interests
 
         bonus = st.toggle(key=f'bonus_toggle_{count}', label=f'Does this account have any periodical bonus? Or will you plan to tap up extra money occacionally?')
         if bonus:
-            col2_1, col2_2 = st.columns(2)
-            with col2_1:
-                _.Bonus.fixed = st.number_input(key=f'bonus_fixed_{count}', label='Fix bonus amount.')
-            with col2_2:
-                _.Bonus.fixed_frequency = int(st.number_input(key=f'bonus_fixed_frequency_{count}', label='How often? (once in XX monthes)'))
-            col3_1, col3_2, col3_3 = st.columns(3)
+            bns = Bonus()
+            col3_1, col3_2 = st.columns(2)
             with col3_1:
-                _.Bonus.rate = st.number_input(key=f'bonus_rate_{count}', label='Bonus rate')
+                bns.fixed = st.number_input(key=f'bonus_fixed_{count}', label='Fix bonus amount.')
             with col3_2:
-                _.Bonus.limit = st.number_input(key=f'bonus_limit_{count}', label='Max amount of bonus')
-            with col3_3:
-                _.Bonus.frequency = int(st.number_input(key=f'bonus_frequency_{count}', label='How often? (once in XX monthes)'))
+                bns.fixed_frequency = int(st.number_input(key=f'bonus_fixed_frequency_{count}', label='How often? (once in XX monthes)'))
+            col4_1, col4_2, col4_3 = st.columns(3)
+            with col4_1:
+                bns.rate = st.number_input(key=f'bonus_rate_{count}', label='Bonus rate')
+            with col4_2:
+                bns.limit = st.number_input(key=f'bonus_limit_{count}', label='Max amount of bonus')
+            with col4_3:
+                bns.frequency = int(st.number_input(key=f'bonus_frequency_{count}', label='How often? (once in XX monthes)'))
 
-        accounts.append(_)
         if count < max_accounts:
             add_more = st.toggle(key=f'tg_{count}',label=f'Add more contribute account')
             
             if add_more:
                 count += 1
-                add_account(count)
+                intr_c += 100
+                accounts.append(acc)
+                add_account(count,intr_c)
         else:
             pass
 
-    add_account(count)
-    
+    add_account(count,intr_c)
+    return accounts
 
 simu: SimulateCondition 
 max_accounts = 8
 max_interest_condition = 3
 
-st.write('### Mulitopult account saving simulattor')
+st.write('### Mulitipul account saving simulator')
 simu = SimulateCondition(st.text_input(key='simulate_name', label=f'Name this simulation', max_chars=20))
-simu.saving_month = int(st.number_input('How long will you save (months)?',min_value=1,step=1))
-accounts:list[Account] = []
-add_accounts()
+col0_1, col0_2, col0_3= st.columns(3)
+with col0_1:
+    saving_year = st.text_input(key='saving_y',label='How long will you save?',max_chars=2,placeholder='year(s)')
+with col0_2:
+    saving_month = st.text_input(key='saving_m',label='month(s)',max_chars=2,placeholder='month(s)')
+
+if (saving_year and saving_month):
+    try:
+        saving_year = int(saving_year)
+        saving_month = int(saving_month)
+        simu.saving_month = (12*saving_month) + saving_month
+    except Exception:
+    
+        st.error('Please put number of month/year')
+st.write('#### Accounts')
+accounts = add_accounts()
 
 
 st.write(simu)
@@ -84,11 +101,7 @@ for i in accounts:
     st.write('account obj')
     st.write(i)
     st.write('interest list')
-    st.write(i.interests)
-    st.write('interest')
-    st.write(i.Interest.rate)
-    st.write('bonus fixed amount')
-    st.write(i.Bonus.fixed)
+    st.write(i.interests[0])
 st.write('number of accounts')
 st.write(len(accounts))
 
--- app/new_schema.py
+++ app/new_schema.py
@@ -1,40 +1,37 @@
-
+class Interest:
+    balance_tier: float = 0
+    rate: float = 0.0
+    limited_period: int = 0
+    
+    def set_interest_rate(self, rate: float):
+        return rate/100
+    
+class Bonus:
+    rate: float = 0.00
+    limit: float = 0.00
+    frequency: int = 0
+    fixed: float = 0.00
+    fixed_frequency: int = 0
     
 class Account:
     name: str
     balance: float
     monthly_add:float = 0.00
-    interest_transfer: bool = False
-    interest_transfer_tgt: str
     earned_interest: float = 0.00
     earned_bonus: float = 0.00
-    interests: dict = {}
+    interest_transfer_tgt: str
+    interests: list[Interest] = []
     interest_periodicity: str
-    
-    class Interest:
-        balance_tier: float = 0
-        rate: float = 0.0
-        limited_period: int = 0
-        
-    class Bonus:
-        rate: float = 0.00
-        limit: float = 0.00
-        frequency: int = 0
-        fixed: float = 0.00
-        fixed_frequency: int = 0
-        
+    bonus: Bonus
     
     def __init__(self, aname):
         self.name = aname
-    
-    def set_interest_rate(self, rate: float):
-        return rate/100
-    
+
+
 class SimulateCondition:
     name:str
     accounts:list[Account]
     saving_month: int
-    contribute_accounts: list[Account] = []
     
     def __init__(self, name:str):
         self.name = name
@@ -61,5 +58,4 @@ class Simulate(SimulateCondition):
             self.saved  = saved
 
 
-# TODO class 名を整理して　利率・利率が与えられる上限額・特別金利・特別金利期間・に対応する
-# 　　
+# TODO classをループで使う方法を探す。ディクショナリにしてみる0
